4c572fb11446a2534bdeec6f267d81153f30f175
Chris <hargrove.chris91@gmail.com>
2018-06-26 22:07:40 +0100

    Goat Charge Script finished.
    Can activate GoatCharge from another script so that the goat will start charging.

--- Assets/ChrisStuff/GoatCharge.cs
+++ Assets/ChrisStuff/GoatCharge.cs
@@ -24,57 +24,66 @@ public class GoatCharge : MonoBehaviour {
     ///     Direction Vector of the Object
     /// </summary>
     private Vector3 m_direction;
-
-    /// <summary>
-    ///     Whether the goat should start to charge or not
-    /// </summary>
-    private bool m_shouldStartCharge;
+    
 
     /// <summary>
     ///     The Target Game Object;
     /// </summary>
     public GameObject m_tObject;
 
-    public float force;
-    public float damping = 1;
-    public float Explosion_Force;
+    public bool     m_isChargeFinished;
+    public float    m_force;
+    private float   m_damping = -0.95f;
+    public float    m_explosionModifier = 4;
+
     
-    void OnCollisionEnter(Collision collision)
-    {
-        float enemyForce = m_tObject.GetComponent<GoatCharge>().force;
-        float explosionForce = (force + enemyForce);
-        
-
-        Rigidbody m_tBody = m_tObject.GetComponent<Rigidbody>();
-
-        Vector2 ratio = new Vector2(explosionForce, enemyForce);
-
-        m_rBody.AddExplosionForce(((explosionForce * Explosion_Force) * damping) * ratio.y, m_tBody.position, 1.0f);
-        m_tBody.AddExplosionForce(((explosionForce * Explosion_Force) * damping) * ratio.x, m_rBody.position, 1.0f);
-
-    }
 
     // Use this for initialization
     void Start() {
         m_transform = GetComponent<Transform>();
         m_direction = (m_tObject.GetComponent<Transform>().position - m_transform.position).normalized;
         m_rBody = GetComponent<Rigidbody>();
-        m_shouldStartCharge = false;
-        
+        m_isChargeFinished = false;
     }
 
     // Update is called once per frame
     void Update() {
         m_direction = (m_tObject.GetComponent<Transform>().position - m_transform.position).normalized;
 
-        if (m_shouldStartCharge) {   
-            m_shouldStartCharge = true;
-        }
-        else
-        {
-            m_rBody.AddForce(m_direction * force);
-        }
+        m_rBody.AddForce(m_direction * m_force);
 
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        
+        float enemyForce = m_tObject.GetComponent<GoatCharge>().m_force;
+        float explosionForce = (m_force + enemyForce);
+        
+
+        Rigidbody m_tBody = m_tObject.GetComponent<Rigidbody>();
+
+        Vector2 ratio = new Vector2(m_force / explosionForce, enemyForce / explosionForce);
+
+        if(ratio.y > ratio.x)
+        {
+            m_tBody.AddExplosionForce(explosionForce * m_explosionModifier * m_damping * ratio.x, m_rBody.position, 1.0f);
+            m_rBody.AddExplosionForce(explosionForce * m_explosionModifier * ratio.y, m_tBody.position, 1.0f);
+        }
+        else
+        {
+            m_tBody.AddExplosionForce(explosionForce * m_explosionModifier * ratio.x, m_rBody.position, 1.0f);
+            m_rBody.AddExplosionForce(explosionForce * m_explosionModifier * m_damping * ratio.y, m_tBody.position, 1.0f);
+        }        
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (m_rBody.velocity.magnitude < 0.39f)
+        {
+            m_isChargeFinished = true;
+            this.enabled = false;
+        }
+    }
 }
