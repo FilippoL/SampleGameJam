cc32558ae2ae98f489b0a26c7bc730687946a441
6Axel9 <oberonx10@gmail.com>
2018-06-27 12:24:46 +0100

    Added fixed animator

--- Assets/ChrisStuff/Scripts/Charge.cs
+++ Assets/ChrisStuff/Scripts/Charge.cs
@@ -36,12 +36,24 @@ public class Charge : MonoBehaviour {
     private float   m_damping = -4.0f;
     public float    m_explosionModifier = 4;
 
+    private void OnEnable()
+    {
+        m_transform = GetComponent<Transform>();
+        if (m_tObject)
+        {
+            m_direction = (m_tObject.GetComponent<Transform>().position - m_transform.position).normalized;
+        }
 
+        GetComponent<Animator>().SetBool("startCharge", true);
+
+        m_rBody = GetComponent<Rigidbody>();
+        m_isChargeFinished = false;
+    }
 
     // Use this for initialization
     void Start() {
         m_transform = GetComponent<Transform>();
-        if(m_tObject != null)
+        if(m_tObject)
         {
             m_direction = (m_tObject.GetComponent<Transform>().position - m_transform.position).normalized;
         }
@@ -70,7 +82,7 @@ public class Charge : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision)
     {
-        if (m_tObject == null)
+        if (!m_tObject)
         {
             return;
         }
@@ -82,6 +94,7 @@ public class Charge : MonoBehaviour {
 
 
         Rigidbody m_tBody = m_tObject.GetComponent<Rigidbody>();
+        m_rBody = GetComponent<Rigidbody>();
 
         Vector2 ratio = new Vector2(m_force / explosionForce, enemyForce / explosionForce);
 
@@ -100,6 +113,7 @@ public class Charge : MonoBehaviour {
 
     private void OnCollisionStay(Collision collision)
     {
+        m_rBody = GetComponent<Rigidbody>();
         if (m_rBody.velocity.magnitude < 0.39f)
         {
             m_isChargeFinished = true;
--- Assets/FilippoStuff/Scripts/PowerBarScript.cs
+++ Assets/FilippoStuff/Scripts/PowerBarScript.cs
@@ -105,15 +105,14 @@ public class PowerBarScript : MonoBehaviour {
 	/// Call this for increasing the value of status bar
 	/// </summary>
 	/// <param name="increase_value">Increasement value.</param>
-	public void Set (float increase_value = 0.5f) {
+	public void Set (float value = 0.0f) {
 
-        m_completeness = increase_value;
+        m_completeness = value;
         //m_progress_bar_img = Sprite.Create(new Texture2D(250,50),new Rect(0.0f, 0.0f, 250, 50), new Vector2(0.5f, 0.5f), 100.0f);
     }   
 
     void Update() {
 
-        //m_completeness = Time.time * 0.05;
     }
 
     void OnGUI()
