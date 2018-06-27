861a4d656a26dfd62687ed9c9a30ae7eb59dd6fe
Chris <hargrove.chris91@gmail.com>
2018-06-27 20:58:02 +0100

    Day Night Cycle Working correctly :) ....

--- Assets/ChrisStuff/Scripts/DayCycle.cs
+++ Assets/ChrisStuff/Scripts/DayCycle.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class DayCycle : MonoBehaviour {
 
-    
     float m_startTime;
 
     public float m_time = 0;
@@ -13,9 +12,14 @@ public class DayCycle : MonoBehaviour {
 
     private Transform m_lightTransform;
 
-    public Material dayTimeMat;
-    public Material nightTimeMat;
-    public float duration = 2.0f;
+    public Color dayTimeTint;
+    public Color nightTimeTint;
+    public float duration = 1.0f;
+    public float lerpAmount = 0;
+
+    public float m_angle;
+    public float offset;
+
 
     [SerializeField]private float m_MAX_INTENSITY;
 
@@ -38,24 +42,21 @@ public class DayCycle : MonoBehaviour {
         m_DayLengthMinutes = 3;
         m_MAX_INTENSITY = GetComponent<Light>().intensity;
         m_startTime = Time.time;
+        m_angle = 0;
+        offset = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        float lerp = Mathf.PingPong(Time.time, duration) / duration;
-
-        Debug.Log("Lerp: " + lerp.ToString());
-        Debug.Log(RenderSettings.skybox.HasProperty("Tint Color").ToString());
-        Debug.Log(RenderSettings.skybox.HasProperty("_TintColor").ToString());
-        
-       
 
         m_lightTransform = GetComponent<Transform>();
 
-        if (m_time > m_DayLengthMinutes * 60.0f)
-        {
+        if (m_time > m_DayLengthMinutes * 60.0f) {
             m_time = 0;
+        }
+        else if( m_time < 0) {
+            m_time = m_DayLengthMinutes * 60.0f;
         }
 
         float modifier = 0.0f;
@@ -70,21 +71,44 @@ public class DayCycle : MonoBehaviour {
             m_lightTransform.rotation = Quaternion.Euler(-Utils.MapRange(m_time, 0.0f, m_DayLengthMinutes * 60.0f, 0.0f, 360.0f), 90.0f, 0);
         }
 
+        m_angle = Utils.MapRange(m_time, 0.0f, m_DayLengthMinutes * 60.0f, 0.0f, 360.0f);
         
-        if ( m_lightTransform.rotation.eulerAngles.x > 180 )
-        {
-            if(GetComponent<Light>().intensity > 0)
-            {
-                GetComponent<Light>().intensity -= Time.deltaTime * modifier;
-            }
-        }
-        else if (m_lightTransform.rotation.eulerAngles.x > 0)
+
+        //Moon Up
+        if(m_angle > 0 && m_angle < 180)
         {
             if (GetComponent<Light>().intensity < m_MAX_INTENSITY)
             {
                 GetComponent<Light>().intensity += Time.deltaTime * modifier;
             }
         }
+        else
+        {
+            if (GetComponent<Light>().intensity > 0)
+            {
+                GetComponent<Light>().intensity -= Time.deltaTime * modifier;
+            }
+        }
+
+        if (m_angle > 0 && m_angle < 20)
+        {
+            lerpAmount = Utils.MapRange(m_angle + offset, 0, 20, 1, 0);
+        }
+        else if(m_angle > 160 && m_angle < 180)
+        {
+            lerpAmount = Utils.MapRange(m_angle - offset, 160, 180, 0, 1);
+        }
+
+        if (CompareTag("Sun"))
+        {
+
+            if (RenderSettings.skybox.HasProperty("_TintColor"))
+                RenderSettings.skybox.SetColor("_TintColor", Color.Lerp(dayTimeTint, nightTimeTint, lerpAmount));
+
+            Debug.Log(m_lightTransform.rotation.eulerAngles.x.ToString());
+            Debug.Log(lerpAmount.ToString());
+        }
+
         m_time += Time.deltaTime;
 	}
 }
